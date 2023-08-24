bc38123254c273644468eb5b02789578ceab53e2
Harshal2742 <harshaltakade1234@gmail.com>
2023-08-24 11:06:08 +0530

    Implemented responsive navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { IoClose, IoMenu } from "react-icons/io5";
+
 const Header = (): JSX.Element => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className="header">
-      <nav className="container">
+      <button
+        className="btn-menu"
+        onClick={() => {
+          setShowMenu(true);
+        }}
+      >
+        <IoMenu className="btn-icon" />
+      </button>
+      <nav className={`container nav-box ${showMenu && "visible"}`}>
+        <button
+          className="btn"
+          onClick={() => {
+            setShowMenu(false);
+          }}
+        >
+          <IoClose className="btn-icon" />
+        </button>
         <ul className="nav-list">
           <li className="get-app">
             <a href="#" className="nav-link">
